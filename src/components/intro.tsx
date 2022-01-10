8686b7fc81b87c557a3f573ad8efb725e87627ce
Siu-Reed <tldn960823@gmail.com>
2022-01-10 15:01:56 +0900

    [add]

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -28,12 +28,12 @@ const Intro:React.FC<Props> = memo(({setIntro}) => {
                 </div>
                 <div className={`${styles.dtdHolder} ${styles.t1}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 101">
-                        <path d="M12.92,83C27.54,53.8,52.8,2.8,52.8,2.8l40,80" transform="translate(-2.3 -2.3)" />
+                        <path d="M12.92,83C27.54,53.8,52.8,2.8,52.8,2.8l40,80" className="dtdTransform" />
                     </svg>
                 </div>
                 <div className={`${styles.dtdHolder} ${styles.e1}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 101">
-                        <path d="M52.92,63a49.92,49.92,0,0,0-40,20,50,50,0,0,0,79.89-.15A49.92,49.92,0,0,0,52.92,63Z" transform="translate(-2.3 -2.3)" />
+                        <path d="M52.92,63a49.92,49.92,0,0,0-40,20,50,50,0,0,0,79.89-.15A49.92,49.92,0,0,0,52.92,63Z" className="dtdTransform" />
                     </svg>
                 </div>
                 <div className={`${styles.dtdHolder} ${styles.d2}`}>
